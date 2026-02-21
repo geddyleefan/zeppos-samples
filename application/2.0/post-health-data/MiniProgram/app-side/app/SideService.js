86d46c5a64370bd955b24b23dd3128d2a7c7bafd
geddyleefan <ciscoguy@hotmail.com>
2026-02-20 20:26:42 -0600

    Implement upload request handling in SideService

--- application/2.0/post-health-data/MiniProgram/app-side/app/SideService.js
+++ application/2.0/post-health-data/MiniProgram/app-side/app/SideService.js
@@ -1,1 +1,17 @@
+AppSideService(
+  BaseSideService({
+    async onRequest(req, res) {
+      const { type, params } = req;
+      if (type === "UPLOAD") {
+        const result = await fetch({
+          url: 'https://zepp-aubrey.azurewebsites.net/sleep',
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(params)
+        });
+        res(null, result.body);
+      }
+    }
+  })
+);
 
